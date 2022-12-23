4cafc69b99a16fdcc0c7422c1feaa5384550259a
Hwikun <m9dnrs8608@daum.net>
2022-12-23 09:03:45 +0900

    Initial Commit

---
+++ ImpulseControlTimer Watch App/ImpulseControlTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpulseControlTimerApp.swift
+//  ImpulseControlTimer Watch App
+//
+//  Created by hwikun on 2022/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ImpulseControlTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImpulseControlTimer/ImpulseControlTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpulseControlTimerApp.swift
+//  ImpulseControlTimer
+//
+//  Created by hwikun on 2022/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ImpulseControlTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
