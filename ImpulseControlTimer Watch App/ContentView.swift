2a600ed2719ea3c2d07429d06809f5419d78218f
Hwikun <leah8608@gmail.com>
2023-01-04 12:17:18 +0900

    adjust watch app

--- ImpulseControlTimer Watch App/ContentView.swift
+++ ImpulseControlTimer Watch App/ContentView.swift
@@ -7,39 +7,69 @@
 
 import SwiftUI
 
+struct Impulse: Codable, Hashable {
+    var title: String
+    var minute: Int
+}
+
+class ImpulseStore: ObservableObject {
+    init() {
+        if let impulsies = UserDefaults.standard.data(forKey: "impulsies") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([Impulse].self, from: impulsies) {
+                self.impulsies = decoded
+                return
+            }
+        }
+        self.impulsies = []
+    }
+
+    @Published var impulsies = [Impulse]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(impulsies) {
+                UserDefaults.standard.set(encoded, forKey: "impulsies")
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var newImpulse: String = ""
-    @State private var impulseArr = UserDefaults.standard.stringArray(forKey: "impulseArr") ?? nil
-    var createImpulseArr: Void = UserDefaults.standard.set(["먹기", "자기", "놀기"], forKey: "impulseArr")
-    @State var goToMain: Bool = false
-    
+    @State private var newMinute: String = ""
+    @FocusState private var isFocused: Bool
+    @ObservedObject private var impulseArr = ImpulseStore()
 
     var body: some View {
-        NavigationStack {
+        NavigationView {
             List {
                 Section {
-                    ForEach(impulseArr ?? [""], id: \.self) { item in
-                        NavigationLink {
-                            TimerView(timerAmount: 3, title: item)
-                        } label: {
-                            HStack {
-                                Text(item)
-                                Spacer()
-                                Text("- 10분")
-                            }
-                        }
+                    ForEach(impulseArr.impulsies, id: \.self) { (item: Impulse) in
+                        listItem(title: item.title, minute: item.minute)
                     }
                     .onDelete(perform: removeList)
+                    .onMove(perform: moveList)
                 }
 
                 Section(header: Text("새 충동")) {
-                    TextField("충동을 입력", text: $newImpulse)
-                        .disableAutocorrection(true)
-                        .onSubmit {
-                            impulseArr?.append(newImpulse)
-                            UserDefaults.standard.set(impulseArr, forKey: "impulseArr")
-                            newImpulse = ""
+                    Group {
+                        TextField("충동을 입력", text: $newImpulse)
+                            .focused($isFocused)
+                            .disableAutocorrection(true)
+
+                        TextField("시간(분)을 입력", text: $newMinute)
+                            .focused($isFocused)
+                            
+
+                        Button("Add") {
+                            if newImpulse != "" || newMinute != "" {
+                                impulseArr.impulsies.append(Impulse(title: newImpulse, minute: Int(newMinute)!))
+                                newMinute = ""
+                                newImpulse = ""
+                                isFocused = false
+                            }
                         }
+                    }
                 }
             }
             .navigationTitle("충동 선택하기")
@@ -47,8 +77,33 @@ struct ContentView: View {
     }
 
     func removeList(at offsets: IndexSet) {
-        impulseArr!.remove(atOffsets: offsets)
-        UserDefaults.standard.set(impulseArr, forKey: "impulseArr")
+        impulseArr.impulsies.remove(atOffsets: offsets)
+//        saveImpulsies(data: impulseArr.impulsies)
+    }
+
+    func moveList(from fromOffSets: IndexSet, to toOffSets: Int) {
+        impulseArr.impulsies.move(fromOffsets: fromOffSets, toOffset: toOffSets)
+//        saveImpulsies(data: impulseArr.impulsies)
+    }
+
+    func saveImpulsies(data: [Impulse]) {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(data) {
+            UserDefaults.standard.set(encoded, forKey: "impulsies")
+        }
+    }
+
+    @ViewBuilder
+    private func listItem(title: String, minute: Int) -> some View {
+        NavigationLink {
+            TimerView(timerAmount: minute * 60, title: title)
+        } label: {
+            HStack {
+                Text(title)
+                Spacer()
+                Text("- \(minute)분")
+            }
+        }
     }
 }
 
--- ImpulseControlTimer Watch App/TimerView.swift
+++ ImpulseControlTimer Watch App/TimerView.swift
@@ -5,9 +5,42 @@
 //  Created by hwikun on 2022/12/30.
 //
 
-import Combine
 import SwiftUI
 import UserNotifications
+
+class NotificationManager: ObservableObject {
+    let notiCenter = UNUserNotificationCenter.current()
+    init() {}
+
+    func requestAuthorization() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { _, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("SUCCESS")
+            }
+        }
+    }
+
+    func scheduleNotification(time: Int) {
+        let content = UNMutableNotificationContent()
+        content.title = "Time is Over!!!"
+        content.body = "Enjoy!!!"
+        content.sound = .default
+        content.badge = 1
+
+        let trigger: UNNotificationTrigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(time), repeats: false)
+
+        let request = UNNotificationRequest(identifier: "timeOut", content: content, trigger: trigger)
+        notiCenter.add(request)
+    }
+
+    func cancelNotification() {
+        notiCenter.removeAllDeliveredNotifications()
+        notiCenter.removeAllPendingNotificationRequests()
+    }
+}
 
 struct TimerView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -17,7 +50,7 @@ struct TimerView: View {
     @State var isOver: Bool = false
     @State var targetTime: Date = .init()
     @Environment(\.dismiss) private var dismiss
-    let notificationCenter = UNUserNotificationCenter.current()
+    @ObservedObject private var manager = NotificationManager()
 
     var body: some View {
         VStack {
@@ -30,51 +63,37 @@ struct TimerView: View {
             if isOver {
                 Button {
                     dismiss()
+                    self.manager.cancelNotification()
                 } label: {
                     Text("돌아가기")
                 }
+                .padding()
             }
         }
         .navigationBarBackButtonHidden()
         .onAppear {
-            targetTime = Calendar.current.date(byAdding: .second, value: timerAmount, to: Date()) ?? Date()
+            targetTime = Date(timeIntervalSinceNow: TimeInterval(timerAmount))
+            self.manager.requestAuthorization()
+            self.manager.scheduleNotification(time: timerAmount)
         }
     }
 
     func calcRemain(targetTime: Date) {
-        let remaining = Calendar.current.dateComponents([.minute, .second], from: Date(), to: targetTime)
-        let minute = remaining.minute ?? 0
-        let second = remaining.second ?? 0
-        if minute == 0 && second == 0 {
+        let remaining = Int(targetTime.timeIntervalSince(Date()))
+        var minute = (remaining % 3600) / 60
+        var second = remaining % 60
+        if minute <= 0 && second <= 0 {
             isOver = true
             timer.upstream.connect().cancel()
+            minute = 0
+            second = 0
         }
         timeRemaining = String(format: "%02i:%02i", minute, second)
-    }
-
-    private func generateUserNotification() {
-        notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
-            if let error = error {
-                print(error)
-            } else {
-                if granted {
-                    let content = UNMutableNotificationContent()
-                    content.title = "Time is Over"
-                    content.body = "Enjoy!!!"
-                    content.badge = 1
-                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0, repeats: false)
-                    let request = UNNotificationRequest(identifier: "Sample Notification", content: content, trigger: trigger)
-                    self.notificationCenter.add(request, withCompletionHandler: nil)
-                } else {
-                    print("Not Granted")
-                }
-            }
-        }
     }
 }
 
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
-        TimerView(timerAmount: 3, title: "afadfas")
+        TimerView(timerAmount: 3, title: "afdsaf")
     }
 }
