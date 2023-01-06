811b22e33cd95e0106aa8f7bc252a5aefe7bbb3d
Hwikun <leah8608@gmail.com>
2023-01-06 09:49:56 +0900

    re-init iOS app

---
+++ ImpulseTimer/ImpulseTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpulseTimerApp.swift
+//  ImpulseTimer
+//
+//  Created by hwikun on 2023/01/06.
+//
+
+import SwiftUI
+
+@main
+struct ImpulseTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
